99e8a3e16c9f589469ed3050177c150de39e0e98
Mariyan Damyanov <marians1md@gmail.com>
2024-03-16 17:48:51 +0200

    Create common app in BookTrack application
    Added basic file structure for the common app in the BookTrack application.

--- booktrack/asgi.py
+++ booktrack/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for BookTrack project.
+ASGI config for booktrack project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookTrack.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booktrack.settings')
 
 application = get_asgi_application()
