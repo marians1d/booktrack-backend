abf2d8f798c833b1d8a0c733c6d96241ef86a877
Mariyan Damyanov <marians1md@gmail.com>
2024-03-17 12:47:06 +0200

    Implement use of environment variables in settings
    This commit introduces the use of django-environ for managing environment variables, making the application more secure. Also changes the way API keys are fetched in the book service. The 'python-dotenv' package is removed as it becomes redundant with 'django-environ'.

--- BookTrack/books/services.py
+++ BookTrack/books/services.py
@@ -1,9 +1,10 @@
 import requests
+from django.conf import settings
 
 
 def search_books(query):
-    api_key = 'YOUR_API_KEY'
-    url = f'https://www.googleapis.com/books/v1/volumes?q={query}'
+    api_key = settings.GOOGLE_BOOKS_API_KEY
+    url = f'https://www.googleapis.com/books/v1/volumes?q={query}&key={api_key}'
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
